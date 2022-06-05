0f15d975da71944e1c27e1be6a51824f7ac0c15d
Tyler Tran <104945572+tylertran349@users.noreply.github.com>
2022-06-05 13:16:07 -0700

    Add files via upload

---
+++ State Capitals Quiz.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+	map<string, string> capitals;
+	capitals.emplace("Alabama", "Montgomery");
+	capitals.emplace("Alaska", "Juneau");
+	capitals.emplace("Arizona", "Phoenix");
+	capitals.emplace("Arkansas", "Little Rock");
+	capitals.emplace("California", "Sacramento");
+	capitals.emplace("Colorado", "Denver");
+	capitals.emplace("Connecticut", "Hartford");
+	capitals.emplace("Delaware", "Dover");
+	capitals.emplace("Florida", "Tallahassee");
+	capitals.emplace("Georgia", "Atlanta");
+	capitals.emplace("Hawaii", "Honolulu");
+	capitals.emplace("Idaho", "Boise");
+	capitals.emplace("Illinois", "Springfield");
+	capitals.emplace("Indiana", "Indianapolis");
+	capitals.emplace("Iowa", "Des Moines");
+	capitals.emplace("Kansas", "Topeka");
+	capitals.emplace("Kentucky", "Frankfort");
+	capitals.emplace("Louisiana", "Baton Rouge");
+	capitals.emplace("Maine", "Augusta");
+	capitals.emplace("Maryland", "Annapolis");
+	capitals.emplace("Massachusetts", "Boston");
+	capitals.emplace("Michigan", "Lansing");
+	capitals.emplace("Minnesota", "St. Paul");
+	capitals.emplace("Mississippi", "Jackson");
+	capitals.emplace("Missouri", "Jefferson City");
+	capitals.emplace("Montana", "Helena");
+	capitals.emplace("Nebraska", "Lincoln");
+	capitals.emplace("Nevada", "Carson City");
+	capitals.emplace("New Hampshire", "Concord");
+	capitals.emplace("New Jersey", "Trenton");
+	capitals.emplace("New Mexico", "Santa Fe");
+	capitals.emplace("New York", "Albany");
+	capitals.emplace("North Carolina", "Raleigh");
+	capitals.emplace("North Dakota", "Bismarck");
+	capitals.emplace("Ohio", "Columbus");
+	capitals.emplace("Oklahoma", "Oklahoma City");
+	capitals.emplace("Oregon", "Salem");
+	capitals.emplace("Pennsylvania", "Harrisburg");
+	capitals.emplace("Rhode Island", "Providence");
+	capitals.emplace("South Carolina", "Columbia");
+	capitals.emplace("South Dakota", "Pierre");
+	capitals.emplace("Tennessee", "Nashville");
+	capitals.emplace("Texas", "Austin");
+	capitals.emplace("Utah", "Salt Lake City");
+	capitals.emplace("Vermont", "Montpelier");
+	capitals.emplace("Virginia", "Richmond");
+	capitals.emplace("Washington", "Olympia");
+	capitals.emplace("West Virginia", "Charleston");
+	capitals.emplace("Wisconsin", "Madison");
+	capitals.emplace("Wyoming", "Cheyenne");
+	string guess;
+	for(auto x : capitals) {
+		cout << "What is the capital of " << x.first << "? ";
+		getline(cin, guess);
+		if(guess == x.second) {
+			cout << "You are correct!" << endl;
+		} else {
+			cout << "Incorrect! The capital of " << x.first << " is: " << x.second << "." << endl;
+		}
+	}
+}
