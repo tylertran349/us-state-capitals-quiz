066588f3f1537ef200a073e0ed6522d11dbca775
Tyler Tran <104945572+tylertran349@users.noreply.github.com>
2022-08-11 13:14:43 -0700

    Add files via upload

--- State Capitals Quiz.cpp
+++ State Capitals Quiz.cpp
@@ -5,58 +5,60 @@ using namespace std;
 
 int main() {
 	map<string, string> capitals;
-	capitals.emplace("Alabama", "Montgomery");
-	capitals.emplace("Alaska", "Juneau");
-	capitals.emplace("Arizona", "Phoenix");
-	capitals.emplace("Arkansas", "Little Rock");
-	capitals.emplace("California", "Sacramento");
-	capitals.emplace("Colorado", "Denver");
-	capitals.emplace("Connecticut", "Hartford");
-	capitals.emplace("Delaware", "Dover");
-	capitals.emplace("Florida", "Tallahassee");
-	capitals.emplace("Georgia", "Atlanta");
-	capitals.emplace("Hawaii", "Honolulu");
-	capitals.emplace("Idaho", "Boise");
-	capitals.emplace("Illinois", "Springfield");
-	capitals.emplace("Indiana", "Indianapolis");
-	capitals.emplace("Iowa", "Des Moines");
-	capitals.emplace("Kansas", "Topeka");
-	capitals.emplace("Kentucky", "Frankfort");
-	capitals.emplace("Louisiana", "Baton Rouge");
-	capitals.emplace("Maine", "Augusta");
-	capitals.emplace("Maryland", "Annapolis");
-	capitals.emplace("Massachusetts", "Boston");
-	capitals.emplace("Michigan", "Lansing");
-	capitals.emplace("Minnesota", "St. Paul");
-	capitals.emplace("Mississippi", "Jackson");
-	capitals.emplace("Missouri", "Jefferson City");
-	capitals.emplace("Montana", "Helena");
-	capitals.emplace("Nebraska", "Lincoln");
-	capitals.emplace("Nevada", "Carson City");
-	capitals.emplace("New Hampshire", "Concord");
-	capitals.emplace("New Jersey", "Trenton");
-	capitals.emplace("New Mexico", "Santa Fe");
-	capitals.emplace("New York", "Albany");
-	capitals.emplace("North Carolina", "Raleigh");
-	capitals.emplace("North Dakota", "Bismarck");
-	capitals.emplace("Ohio", "Columbus");
-	capitals.emplace("Oklahoma", "Oklahoma City");
-	capitals.emplace("Oregon", "Salem");
-	capitals.emplace("Pennsylvania", "Harrisburg");
-	capitals.emplace("Rhode Island", "Providence");
-	capitals.emplace("South Carolina", "Columbia");
-	capitals.emplace("South Dakota", "Pierre");
-	capitals.emplace("Tennessee", "Nashville");
-	capitals.emplace("Texas", "Austin");
-	capitals.emplace("Utah", "Salt Lake City");
-	capitals.emplace("Vermont", "Montpelier");
-	capitals.emplace("Virginia", "Richmond");
-	capitals.emplace("Washington", "Olympia");
-	capitals.emplace("West Virginia", "Charleston");
-	capitals.emplace("Wisconsin", "Madison");
-	capitals.emplace("Wyoming", "Cheyenne");
+	capitals["Alabama"] = "Montgomery";
+	capitals["Alaska"] = "Juneau";
+	capitals["Arizona"] = "Phoenix";
+	capitals["Arkansas"] = "Little Rock";
+	capitals["California"] = "Sacramento";
+	capitals["Colorado"] = "Denver";
+	capitals["Connecticut"] = "Hartford";
+	capitals["Delaware"] = "Dover";
+	capitals["Florida"] = "Tallahassee";
+	capitals["Georgia"] = "Atlanta";
+	capitals["Hawaii"] = "Honolulu";
+	capitals["Idaho"] = "Boise";
+	capitals["Illinois"] = "Springfield";
+	capitals["Indiana"] = "Indianapolis";
+	capitals["Iowa"] = "Des Moines";
+	capitals["Kansas"] ="Topeka";
+	capitals["Kentucky"] = "Frankfort";
+	capitals["Louisiana"] = "Baton Rouge";
+	capitals["Maine"] = "Augusta";
+	capitals["Maryland"] = "Annapolis";
+	capitals["Massachusetts"] = "Boston";
+	capitals["Michigan"] = "Lansing";
+	capitals["Minnesota"] = "St. Paul";
+	capitals["Mississippi"] = "Jackson";
+	capitals["Missouri"] = "Jefferson City";
+	capitals["Montana"] = "Helena";
+	capitals["Nebraska"] = "Lincoln";
+	capitals["Nevada"] = "Carson City";
+	capitals["New Hampshire"] = "Concord";
+	capitals["New Jersey"] = "Trenton";
+	capitals["New Mexico"] = "Santa Fe";
+	capitals["New York"] = "Albany";
+	capitals["North Carolina"] = "Raleigh";
+	capitals["North Dakota"] = "Bismarck";
+	capitals["Ohio"] = "Columbus";
+	capitals["Oklahoma"] = "Oklahoma City";
+	capitals["Oregon"] = "Salem";
+	capitals["Pennsylvania"] = "Harrisburg";
+	capitals["Rhode Island"] = "Providence";
+	capitals["South Carolina"] = "Columbia";
+	capitals["South Dakota"] = "Pierre";
+	capitals["Tennessee"] = "Nashville";
+	capitals["Texas"] = "Austin";
+	capitals["Utah"] = "Salt Lake City";
+	capitals["Vermont"] = "Montpelier";
+	capitals["Virginia"] = "Richmond";
+	capitals["Washington"] = "Olympia";
+	capitals["West Virginia"] = "Charleston";
+	capitals["Wisconsin"] = "Madison";
+	capitals["Wyoming"] = "Cheyenne";
 	string option, guess;
+	int correctAnswers;
 	while(option != "capitals" || option != "states") {
+		correctAnswers = 0;
 		cout << "Would you like to answer with capitals or states (enter \"capitals\" or \"states\")? ";
 		getline(cin, option);
 		for(int i = 0; i < option.length(); i++) {
@@ -68,20 +70,24 @@ int main() {
 				getline(cin, guess);
 				if(guess == x.second) {
 					cout << "You are correct!" << endl;
+					correctAnswers++;
 				} else {
 					cout << "Incorrect! The capital of " << x.first << " is: " << x.second << endl;
 				}
 			}
+			cout << "Score: " << correctAnswers << "/50" << endl;
 		} else if(option == "states") {
 			for(auto x : capitals) {
 				cout << "What state is " << x.second << " the capital of? ";
 				getline(cin, guess);
 				if(guess == x.first) {
 					cout << "You are correct!" << endl;
+					correctAnswers++;
 				} else {
 					cout << "Incorrect! " << x.second << " is the capital of: " << x.first << endl;
 				}
 			}
+			cout << "Score: " << correctAnswers << "/50" << endl;
 		}
 	}
 	return 0;
